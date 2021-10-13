eb1e5a31568f8e285551d1709a8be92fa8a1c9b7
wonjun3991 <aa3991@office.uc.ac.kr>
2021-10-13 22:54:51 +0900

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
